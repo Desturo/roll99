6c267786642a5163e60a15189f3fd244e9b34b46
Desturo <lolomat25@gmail.com>
2021-09-19 04:07:33 +0200

    bodyparset

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 
-import characterRoutes from './routes/characters';
+import characterRoutes from './routes/characters.js';
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.json());
 
 app.use('/characters', characterRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello to the roll99 APi');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 })
--- server/routes/characters.js
+++ server/routes/characters.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getCharacters } from '../controllers/characters.js';
 
 const router = express.Router();
 
