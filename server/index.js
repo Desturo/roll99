92ff9d4e946a5050bfd3f65edc68469dc13266f3
Desturo <lolomat25@gmail.com>
2021-09-19 04:02:09 +0200

    simple router

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
+import characterRoutes from './routes/characters';
+
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded());
 
-app.use('/characters', characterRoutes)
+app.use(bodyParser.json());
+
+app.use('/characters', characterRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to the roll99 APi');
+})
