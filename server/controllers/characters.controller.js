fb9aeeac261c173f6e16ee9d0108440eeac9f451
Desturo <lolomat25@gmail.com>
2021-09-19 04:46:58 +0200

    getCharacters now gets all

--- server/controllers/characters.controller.js
+++ server/controllers/characters.controller.js
@@ -3,39 +3,50 @@ import mongoose from 'mongoose'
 import CharaterModel from '../models/character.model.js';
 
 export const getCharacters = async (req, res) => {
-
-    const character = {
-        firstName: 'Chrome',
-        lastName: 'Iridel',
-        creator: 'Desturo',
-        race: 'Elf',
-        origin: 'Dark',
-        abilities: [{
-            name: 'Elemental Magic',
-            value: 78
-        },{
-            name: 'Sword Art',
-            value: 60
-        }],
-        attributes: {
-            strength: 13,
-            endurance: 10,
-            agility: 9,
-            intelligence: 10,
-            magic: 13,
-            speed: 9
-        }
-    };
-
-    const newCharacter = new CharaterModel(character);
-
     try {
-        newCharacter.save((error) => {
-            if (error) return console.log(error);
-        });
+        const characters = await CharaterModel.find();
 
-        res.status(201).json(newCharacter);
+        res.status(200).json(characters);
+        
     } catch (error) {
         res.status(404).json({ message: error });
     }
 }
+//-----------adding character-----------
+// export const getCharacters = async (req, res) => {
+
+//     const character = {
+//         firstName: 'Chrome',
+//         lastName: 'Iridel',
+//         creator: 'Desturo',
+//         race: 'Elf',
+//         origin: 'Dark',
+//         abilities: [{
+//             name: 'Elemental Magic',
+//             value: 78
+//         },{
+//             name: 'Sword Art',
+//             value: 60
+//         }],
+//         attributes: {
+//             strength: 13,
+//             endurance: 10,
+//             agility: 9,
+//             intelligence: 10,
+//             magic: 13,
+//             speed: 9
+//         }
+//     };
+
+//     const newCharacter = new CharaterModel(character);
+
+//     try {
+//         newCharacter.save((error) => {
+//             if (error) return console.log(error);
+//         });
+
+//         res.status(201).json(newCharacter);
+//     } catch (error) {
+//         res.status(404).json({ message: error });
+//     }
+// }
