67e414fe54409a871bc5353d0ebf1dca07f392fb
Desturo <lolomat25@gmail.com>
2021-09-25 04:56:52 +0200

    navbar tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { React, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Form from './components/Form/Form.jsx';
 import Input from './components/Input/Input.jsx';
 import UserForm from './components/UserForm/UserFrom.jsx';
 import LoginForm from './components/LoginForm/LoginForm.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Home from './pages/index.jsx';
 
 import { socket } from './services/socket.js';
 import { generate } from 'random-key';
@@ -29,7 +32,7 @@ function App() {
   }, [messages]);
 
   return (
-    <div>
+    <Router>
       {/* <Form></Form>
       <Input sendMessage={ sendMessage } message={ message } setMessage={ setMessage }></Input>
       <ul>
@@ -40,11 +43,16 @@ function App() {
             )
           })
         }
-      </ul> */}
+      </ul>
 
       <UserForm></UserForm>
-      <LoginForm setLoggedIn={ setLoggedIn }></LoginForm>
-    </div>
+      <LoginForm setLoggedIn={ setLoggedIn }></LoginForm> */}
+      <NavBar>
+          <Switch>
+            <Route path="/" exact component={ Home } />
+          </Switch>
+      </NavBar>
+    </Router>
   );
 }
 
