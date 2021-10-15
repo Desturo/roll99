fb41db08ab532658b7659819d8d2c389c746dcfb
Desturo <lolomat25@gmail.com>
2021-10-15 13:30:06 +0200

    formatted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,47 @@
-import { React, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { React, useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 
-import './App.css';
+import "./App.css";
 
-import Form from './components/Form/Form.jsx';
-import Input from './components/Input/Input.jsx';
-import UserForm from './components/UserForm/UserFrom.jsx';
-import LoginForm from './components/LoginForm/LoginForm.jsx';
+import Form from "./components/Form/Form.jsx";
+import Input from "./components/Input/Input.jsx";
+import UserForm from "./components/UserForm/UserFrom.jsx";
+import LoginForm from "./components/LoginForm/LoginForm.jsx";
 
-
-import { socket } from './services/socket.js';
-import { generate } from 'random-key';
-
-
+import { socket } from "./services/socket.js";
+import { generate } from "random-key";
 
 function App() {
-
   const [message, setMessage] = useState();
 
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const sendMessage = () => {
-    socket.emit('toServer', message);
-  }
-  const [messages, setMessages] = useState([])
-  
+    socket.emit("toServer", message);
+  };
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    socket.once('toClient', (messageObject) => {
-      setMessages([...messages, messageObject])
-      console.log(`${messageObject.id.substr(0, 4)} said ${messageObject.text}`);
+    socket.once("toClient", (messageObject) => {
+      setMessages([...messages, messageObject]);
+      console.log(
+        `${messageObject.id.substr(0, 4)} said ${messageObject.text}`
+      );
     });
   }, [messages]);
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
+        <button
+          onClick={() => {
+            Cookies.set("foo", "bar", { expires: 99 });
+            Cookies.get("foo");
+          }}
+        >
+          Test
+        </button>
         {/* <Form></Form>
         <Input sendMessage={ sendMessage } message={ message } setMessage={ setMessage }></Input>
         <ul>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import { Server } from 'socket.io';
-import { createServer } from 'http'
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import dotenv from "dotenv";
 
-import characterRoutes from './routes/characters.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import characterRoutes from "./routes/characters.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
@@ -15,39 +15,41 @@ dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {cors: {
-    origin: "*"
-}});
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/characters", characterRoutes);
+app.use("/auth", authRoutes);
 
-app.use('/characters', characterRoutes);
-app.use('/auth', authRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Hello to the roll99 APi');
+app.get("/", (req, res) => {
+  res.send("Hello to the roll99 APi");
 });
 
-io.on('connection', (socket) => {
-    console.log(`connection ${socket.id.substr(0, 4)}`)
+io.on("connection", (socket) => {
+  console.log(`connection ${socket.id.substr(0, 4)}`);
 
-    socket.on('toServer', (message) => {
-        const mssageObject = { text: message, id: socket.id};
-        io.emit('toClient', mssageObject);
-    });
+  socket.on("toServer", (message) => {
+    const mssageObject = { text: message, id: socket.id };
+    io.emit("toClient", mssageObject);
+  });
 });
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('connected to database');
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 server.listen(PORT, () => {
-    console.log(`running on port ${PORT}`);
-})
+  console.log(`running on port ${PORT}`);
+});
